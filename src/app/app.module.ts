9fb6e3135a60ec78077dd2346df722e7cdd4a9d0
John Laws Harrison <johnlawsharrison@gmail.com>
2018-02-08 12:32:34 -0800

    Import basic material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+// import material components to be used
+// TODO: material components can be imported in a separate module, to be imported in app module
+import { MatSidenavModule, MatToolbarModule } from '@angular/material';
+
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
