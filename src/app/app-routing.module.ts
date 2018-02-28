14f7035aa03c13e50434f9cdea5563fc626db926
John Laws Harrison <johnlawsharrison@gmail.com>
2018-02-28 13:00:25 -0800

    Add default redirect route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LibraryComponent } from './library/library.component';
 
 const routes: Routes = [
-  {path: 'library', component: LibraryComponent}
+  {path: 'library', component: LibraryComponent},
+  {path: '', redirectTo: 'library', pathMatch: 'full'}
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
