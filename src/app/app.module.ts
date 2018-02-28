c3616cc44b960f8155a42e6a09f6e068aa9856c4
John Laws Harrison <johnlawsharrison@gmail.com>
2018-02-28 13:38:20 -0800

    Generate search-box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { AppRoutingModule } from './/app-routing.module';
 import { LibraryComponent } from './library/library.component';
 import { CategoryComponent } from './category/category.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 
 @NgModule({
@@ -38,8 +39,9 @@ import { SearchResultsComponent } from './search-results/search-results.componen
     ShelfDetailComponent,
     ShelfEditorComponent,
     LibraryComponent,
-    CategoryComponent
-    SearchResultsComponent
+    CategoryComponent,
+    SearchResultsComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule,
