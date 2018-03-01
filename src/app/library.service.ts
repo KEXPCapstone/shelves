23f16d3c41186113c583028c59739c9d719bbec7
John Laws Harrison <johnlawsharrison@gmail.com>
2018-03-01 12:02:35 -0800

    Create basic error handler for library service

--- src/app/library.service.ts
+++ src/app/library.service.ts
@@ -7,9 +7,32 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 import { Release } from './release';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable()
 export class LibraryService {
 
-  constructor() { }
+  private libraryUrl = 'api/library'; // URL to web api
 
+  constructor(private http: HttpClient) { }
+
+  private log(message: string) {
+    // TODO: better logging, don't log to console
+    console.log(message);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      //TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // let the app keep running by returning an empty result
+      return of(result as T);
+    };
+  }
 }
