876b210a2eff73955b8371f8a9c041b8a6a1751f
John Laws Harrison <johnlawsharrison@gmail.com>
2018-03-01 11:55:35 -0800

    Define release class

---
+++ src/app/release.ts
@@ -0,0 +1,9 @@
+// Release class represents a single release in the KEXP library
+export class Release {
+  id: number;
+  title: string;
+  artistName: string;
+  labelName: string;
+  kexpCategory: string;
+  // releaseDate, tracklist, imageUrl, ...
+}
