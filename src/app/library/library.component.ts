82d8acbf6eadd577c6c114281f8ae6b94684c3d8
John Laws Harrison <johnlawsharrison@gmail.com>
2018-03-01 13:52:54 -0800

    Create routerlinks to category component

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class LibraryComponent implements OnInit {
 
   categories = [
-    'rock/pop',
-    'hip hop',
+    'rock-pop',
+    'hiphop',
     'local',
     'world',
     'roots',
     'reggae',
-    'beats/electronic'
+    'beats-electronic'
   ];
 
   constructor() { }
