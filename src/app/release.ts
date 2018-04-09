3d487718bd48f1cf1f86195f120f91b22934a415
John Laws Harrison <johnlawsharrison@gmail.com>
2018-04-08 18:12:21 -0700

    Update release model

--- src/app/release.ts
+++ src/app/release.ts
@@ -1,15 +1,24 @@
 // Release class represents a single release in the KEXP library
 export class Release {
-  id: number;
-  title: string;
-  artistName: string;
-  labelName: string;
-  kexpCategory: string;
-  trackList: string[]; // what do we need to store tracks as?
-  releaseDate: Date;
-  imageUrl: string;
-  releaseMBID: string;
-  daletLibraryCategoryID: string; // the 'id' of the primary category under which this release is filed in dalet
-  releaseLength: Date; // hh:mm:ss
+  id: string; // currently 'KEXPMBID', but required by in memory test API to be named 'id'
+  KEXPReleaseGroupMBID: string;
+  KEXPPrimaryGenre: string;
+  KEXPCountryCode: string;
+  KEXPDateReleased: string; // may end up being date (if reformatted)
+  KEXPReleaseArtistCredit: string;
+  KEXPArtist_KEXPSortName: string;
+  KEXPFirstReleaseDate: string; // or Date
+  KEXPTitle: string;
+  // title: string;
+  // artistName: string;
+  // labelName: string;
+  // kexpCategory: string;
+  // trackList: string[]; // what do we need to store tracks as?
+  // releaseDate: Date;
+  // imageUrl: string;
+  // releaseMBID: string;
+  // daletLibraryCategoryID: string; // the 'id' of the primary category under which this release is filed in dalet
+  // releaseLength: Date; // hh:mm:ss
   // more fields ...
 }
+
