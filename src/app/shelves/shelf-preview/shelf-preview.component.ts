77abadb9f815b8322ab24206a1394114eacd2056
John Laws Harrison <johnlawsharrison@gmail.com>
2018-04-27 22:26:09 -0700

    Create shelves module

---
+++ src/app/shelves/shelf-preview/shelf-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shelf-preview',
+  templateUrl: './shelf-preview.component.html',
+  styleUrls: ['./shelf-preview.component.css']
+})
+export class ShelfPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
