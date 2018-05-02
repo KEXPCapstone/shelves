2d69e59668dc09aaf6ad97f3d58ed7955d40376e
Adam Bourn <abourn@uw.edu>
2018-05-01 20:37:53 -0700

    Add basic sign in page, start attempt at error handling for login in auth.service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LibraryComponent } from './library/library.component';
 import { CategoryComponent } from './category/category.component';
 import { ReleaseDetailComponent } from './release/release-detail/release-detail.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/library', pathMatch: 'full'}, // default route redirect
   {path: 'library', component: LibraryComponent}, // home page
   {path: 'shelves', component: LibraryComponent}, // shelves feature module
-  {path: 'library/releases/:releaseId', component: ReleaseDetailComponent} // release detail route
+  {path: 'library/releases/:releaseId', component: ReleaseDetailComponent}, // release detail route
+  {path: 'login', component: SigninComponent} // sign in page
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,19 @@ import { User } from '../user';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../environments/environment';
 import { Credentials } from 'crypto';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { tap, catchError } from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
+
+const fullHttpOptions = {
+    observe: 'response',
+}
+
 
 @Injectable()
 export class AuthService {
@@ -30,9 +36,9 @@ export class AuthService {
     // See: https://angular.io/guide/http
     public login(creds : Credentials): Observable<HttpResponse<User>> {
         const url = `${environment.apiUrl}/sessions`
-        return this.http.post<HttpResponse<User>>(url, creds, httpOptions).pipe(
+        return this.http.post<HttpResponse<User>>(url, creds, {observe: 'response'}).pipe(
             tap(_ => this.log(`logged in user`)),
-            catchError(this.handleError<HttpResponse<User>>('login'))
+            catchError(this.handleError)
         );
     }
 
@@ -41,16 +47,33 @@ export class AuthService {
         console.log(message);
     }
 
-    private handleError<T> (operation = 'operation', result?: T) {
-        return (error: any): Observable<T> => {
-          // TODO: send the error to remote logging infrastructure
-          console.error(error); // log to console instead
+    // private handleError<T> (operation = 'operation', result?: T) {
+    //     return (error: any): Observable<T> => {
+    //       // TODO: send the error to remote logging infrastructure
+    //       console.error(error); // log to console instead
     
-          // TODO: better job of transforming error for user consumption
-          this.log(`${operation} failed: ${error.message}`);
+    //       // TODO: better job of transforming error for user consumption
+    //       this.log(`${operation} failed: ${error.message}`);
     
-          // let the app keep running by returning an empty result
-          return of(result as T);
-        };
-      }
+    //       // let the app keep running by returning an empty result
+    //       return of(result as T);
+    //     };
+    //   }
+
+    private handleError(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+          // A client-side or network error occurred. Handle it accordingly.
+          console.error('An error occurred:', error.error.message);
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          console.error(
+            `Backend returned code ${error.status}, ` +
+            `body was: ${error.error}`);
+        }
+        // return an ErrorObservable with a user-facing error message
+        return new ErrorObservable(
+            `Backend returned code ${error.status}, ` +
+            `body was: ${error.error}`);
+      };
 }
