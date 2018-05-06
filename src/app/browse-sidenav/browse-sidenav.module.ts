7ac852c7f4f6ae76a94ef701a93639cdd06d5cc9
John Laws Harrison <johnlawsharrison@gmail.com>
2018-05-06 14:49:34 -0700

    Implement sticky browse sidenav

--- src/app/browse-sidenav/browse-sidenav.module.ts
+++ src/app/browse-sidenav/browse-sidenav.module.ts
@@ -1,10 +1,11 @@
-import { NgModule, Component, ViewEncapsulation, OnInit } from '@angular/core';
-import { MatSidenavModule } from '@angular/material';
-import { RouterModule } from '@angular/router';
+import { NgModule, Component, ViewEncapsulation, OnInit, NgZone, ViewChild } from '@angular/core';
+import { MatSidenavModule, MatSidenav } from '@angular/material';
+import { RouterModule, ActivatedRoute, Router } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
 import {CdkAccordionModule} from '@angular/cdk/accordion';
 
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
     selector: 'app-browse-sidenav',
@@ -13,8 +14,26 @@ import {CdkAccordionModule} from '@angular/cdk/accordion';
     encapsulation: ViewEncapsulation.None,
 })
 export class BrowseSidenavComponent implements OnInit {
+    private mediaMatcher: MediaQueryList = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px`);
+
+    constructor(private _route: ActivatedRoute,
+                private _router: Router,
+                zone: NgZone) {
+        this.mediaMatcher.addListener(mql => zone.run(() => this.mediaMatcher = mql));
+    }
+
+    @ViewChild(MatSidenav) sidenav: MatSidenav;
 
     ngOnInit() {
+        this._router.events.subscribe((() => {
+            if (this.isScreenSmall()) {
+                this.sidenav.close();
+            }
+        }));
+    }
+
+    isScreenSmall(): boolean {
+        return this.mediaMatcher.matches;
     }
 }
 
