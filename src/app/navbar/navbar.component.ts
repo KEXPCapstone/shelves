c8db79e2ab3c6af5cbff6830b0ffc858fb8aeddf
Adam Bourn <abourn@uw.edu>
2018-05-08 15:50:58 -0700

    First attempt at conditional header--does not update once logged in

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,18 +7,32 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+  private isAuthenticated;
 
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
+    this.checkAuth();
   }
 
   logout() {
     this.authService.logout()
       .subscribe(_ => {
         console.log('signed out');
+        this.isAuthenticated = false;
       }, (error) => {
         console.log(error);
+      }
+    );
+  }
+
+  checkAuth() {
+    this.authService.getCurrUser()
+      .subscribe((resp) => {
+        console.log(resp);
+        this.isAuthenticated = true;
+      }, (error) => {
+        this.isAuthenticated = false;
       }
     );
   }
