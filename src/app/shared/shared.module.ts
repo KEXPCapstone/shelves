536b18a8c1e7c167ce19f1a0175281340eb29d0a
John Laws Harrison <johnlawsharrison@gmail.com>
2018-05-12 15:29:05 -0700

    Stop using FlexLayout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material/material.module';
 import { MatInputModule } from '@angular/material';
 
@@ -11,7 +10,6 @@ import { MatInputModule } from '@angular/material';
     exports: [
         CommonModule,
         MaterialModule,
-        FlexLayoutModule,
         MatInputModule
         // other shared components/directives etc
     ]
