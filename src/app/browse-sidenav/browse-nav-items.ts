fe4855b3473f5024f1c6be2cbf4be09abe05535a
John Laws Harrison <johnlawsharrison@gmail.com>
2018-05-12 15:01:45 -0700

    Use navigation constant

---
+++ src/app/browse-sidenav/browse-nav-items.ts
@@ -0,0 +1,29 @@
+ export interface BrowseCategory {
+    id: string;
+    name: string;
+    groups: string[];
+}
+
+export const BROWSE_NAV_ITEMS: BrowseCategory[] = [
+    {
+        id: 'artists',
+        name: 'Artists',
+        groups: getAlphabeticalGroups()
+    },
+    {
+        id: 'labels',
+        name: 'Labels',
+        groups: getAlphabeticalGroups()
+    },
+    {
+        id: 'decades',
+        name: 'Decades',
+        groups: [
+        ]
+    }
+];
+
+function getAlphabeticalGroups(): string[] {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    return alphabet.split('').slice();
+}
