9edd0b05ce4121d5a3b21d079bd59a40597ac4d8
John Laws Harrison <johnlawsharrison@gmail.com>
2018-05-16 13:05:32 -0700

    Style the fallback image on 404

--- src/app/shared/fallback.directive.ts
+++ src/app/shared/fallback.directive.ts
@@ -8,8 +8,7 @@ export class FallbackDirective {
 
     constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-    @HostListener('error') onError(eventData: Event) {
-        console.log('there was an error!');
-        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    @HostListener('error', ['$event']) onError(event: Event) {
+        // this.renderer.createElement(this.elementRef.nativeElement.parentNode, 'div');
     }
 }
