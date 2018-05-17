556cab94fcd061f9f5c59807ce5fd51324261834
John Laws Harrison <johnlawsharrison@gmail.com>
2018-05-16 19:20:17 -0700

    Create artist and label models

---
+++ src/app/models/label.ts
@@ -0,0 +1,14 @@
+export class Label {
+    id: string;
+    name: string;
+    sortName: string;
+    disambiguation: string;
+    releases: LabelRelease[];
+}
+
+export class LabelRelease {
+    catalogNumber: string;
+    releaseGroupId: string;
+    releaseId: string;
+    title: string;
+}
