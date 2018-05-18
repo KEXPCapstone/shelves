3e4504756780aadd090342d31de0d6947568e99d
Adam Bourn <abourn@uw.edu>
2018-05-17 19:23:46 -0700

    Get release group image if release errs (ends up getting release group for all)

--- src/app/shelves/shelf-preview/shelf-preview.component.ts
+++ src/app/shelves/shelf-preview/shelf-preview.component.ts
@@ -11,10 +11,14 @@ import { LibraryService } from '../../library.service';
 export class ShelfPreviewComponent implements OnInit {
   @Input() shelf: Shelf;
   @Input() clickable: boolean;
+  private imgUrls = new Map();
 
   constructor(private libraryService: LibraryService) { }
 
   ngOnInit() {
+    this.shelf.ReleaseIds.forEach((mbid) => {
+      this.imgUrls.set(mbid, `//coverartarchive.org/release/${mbid}/front-500.jpg`);
+    });
   }
 
   getArt(mbid: string) {
@@ -25,6 +29,9 @@ export class ShelfPreviewComponent implements OnInit {
     console.log(mbid);
     this.libraryService.getReleaseById(mbid)
       .subscribe((release) => {
+        console.log('got backup art!');
+        console.log(release);
+        this.imgUrls.set(release.id, `//coverartarchive.org/release-group/${release.KEXPReleaseGroupMBID}/front-500.jpg`);
         return `//coverartarchive.org/release-group/${release.KEXPReleaseGroupMBID}/front-500.jpg`;
       });
   }
