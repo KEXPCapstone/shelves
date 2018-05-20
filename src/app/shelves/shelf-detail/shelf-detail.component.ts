a62acbf35d2c7e5571b8a5e63ba04a905cb9960e
Adam Bourn <abourn@uw.edu>
2018-05-19 17:00:06 -0700

    Generate shelf detail components

---
+++ src/app/shelves/shelf-detail/shelf-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shelf-detail',
+  templateUrl: './shelf-detail.component.html',
+  styleUrls: ['./shelf-detail.component.scss']
+})
+export class ShelfDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
