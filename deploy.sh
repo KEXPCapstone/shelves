9ed58082fd457f00594706a4f4291db64595845e
John Laws Harrison <johnlawsharrison@gmail.com>
2018-05-21 21:54:27 -0700

    Update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 . build.sh
 echo "Deploying to DigitalOcean Droplet; You may be prompted to enter your SSH passphrase:"
 ssh root@kexpshelves.com << HERE
-    docker pull kexpcapstone/shelves-app
     docker rm -f shelves-app
+    yes | docker image prune -a
+    docker pull kexpcapstone/shelves-app
     docker run -d \
     --name shelves-app \
     -p 80:80 -p 443:443 \
